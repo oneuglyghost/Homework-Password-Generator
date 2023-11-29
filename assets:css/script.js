9d134d47e4864789f45bd6c3373ef35c186e62be
Bernardo Alonso <bernardo4430@gmail.com>
2023-11-28 19:40:47 -0600

    fixed all UI elements with css

--- assets:css/script.js
+++ assets:css/script.js
@@ -4,6 +4,9 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,5 +16,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
